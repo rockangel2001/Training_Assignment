534ae1efe00a734b7b91828cad53ad17e316ccfa
Shreya Mishra <94891746+rockangel2001@users.noreply.github.com>
2023-09-15 23:54:07 +0530

    Add files via upload

---
+++ Assignment/Assignment-5/Assignment5a/Assignment-5/Assignment5b.cs
@@ -0,0 +1,43 @@
+﻿/*2. Create a Program to count the no. of occurrences of a letter in a given string
+(for example in a string called “OOPS PROGRAMMING”, O appears 3 times)
+Hint: Accept a string and also the letter to be counted 
+--------------------------------------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    class Assignment5b
+    {
+        static void Main()
+        {
+
+            Countletter();
+
+            Console.Read();
+        }
+        public static int Countletter()
+        {
+            Console.WriteLine("Enter your sentence: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Enter your character you want to count: ");
+            char a = Convert.ToChar(Console.ReadLine());
+
+            int count = 0;
+            foreach (char i in b)
+            {
+                if (i == a)
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine("My count is : " + count);
+            return count;
+        }
+    }
+}
+    
