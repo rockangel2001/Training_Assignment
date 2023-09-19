d5a02f832822c20d27021f15c1179086f6acc908
Shreya Mishra <94891746+rockangel2001@users.noreply.github.com>
2023-09-19 11:00:56 +0530

    Add files via upload
    Test_2(CodeBase)

---
+++ Assignment/Test_2/Test_2/Test_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+        public abstract bool IsPassed(double grade);
+
+    }
+
+    class Undergraduate : Student
+
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+            Undergraduate undergradStudent = new Undergraduate();
+
+            undergradStudent.Name = "Shreya";
+
+            undergradStudent.StudentId = 2001;
+
+            undergradStudent.Grade = 87.0;
+
+            Graduate gradStudent = new Graduate();
+
+            gradStudent.Name = "Prachi";
+
+            gradStudent.StudentId = 1002;
+
+            gradStudent.Grade = 78.0;
+
+            Console.WriteLine($"{undergradStudent.Name} passed: {undergradStudent.IsPassed(undergradStudent.Grade)}");
+            Console.WriteLine($"{gradStudent.Name} passed: {gradStudent.IsPassed(gradStudent.Grade)}");
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ Assignment/Test_2/Test_2/Test_2/test2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2
+{
+    class Product
+    {
+        public int ProductId;
+        public string ProductName;
+        public double Price;
+    }
+    class test2
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+
+
+            // doing entry of 10 products
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i}:");
+                Product product = new Product();
+
+
+
+                Console.Write("Product ID: ");
+                product.ProductId = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.Write("Product Name: ");
+                product.ProductName = Console.ReadLine();
+
+
+
+                Console.Write("Price: ");
+                product.Price = Convert.ToDouble(Console.ReadLine());
+
+
+
+                products.Add(product);
+
+
+
+
+            }
+
+
+
+            // Sorting the products based on there price
+            List<Product> sortedProducts = products.OrderBy(p => p.Price).ToList();
+
+
+
+            // Displaying the sorted products
+            Console.WriteLine("\nSorted Products by Price:");
+            foreach (var product in sortedProducts)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Price: {product.Price}");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
+
+
+
