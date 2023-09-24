0ad164b1d0b9443f0c528768fbd5d3781d7f9d7a
Shreya Mishra <shreyam@infinite.com>
2023-09-24 14:15:35 +0530

    Assignment-6

---
+++ Assignment/Assignment-6/Assignment-6/Assignment-6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concession_Library;
+
+namespace Assignment_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TotalFare = 500;
+
+            Class1 c = new Class1();
+            Console.WriteLine("Enter your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Age ");
+            uint age = uint.Parse(Console.ReadLine()); //when we need positive no. only we use uint.
+            c.CalculateConcession(name, age);
+            Console.Read();
+        }
+    }
+}
