5688c50190bea16a7be196bb0e3717d59bc40c62
Shreya Mishra <shreyam@infinite.com>
2023-10-19 10:00:21 +0530

    SQL_ASSIGNMENT_5

--- SQL/Assignment5.sql
+++ SQL/Assignment5.sql
@@ -1,55 +1,51 @@
---1.    Write a T-SQL based procedure Program to generate complete payslip of a given employee with respect to the following condition
-
---a)	HRA  as 10% Of sal
---b)	DA as  20% of sal
---c)	PF as 8% of sal
---d)	IT as 5% of sal
---e)	Deductions as sum of PF and IT
---f)	Gross Salary as sum of SAL,HRA,DA 
---g)	Net salary as  Gross salary- Deduction
-
-
-
 Create Database Assignment5
-use Assignment5
+use Assignment2
 
-Create procedure GeneratePayslip
-    @EmployeeID int,
-    @Salary decimal(10, 2)
+-- Creating procedure
+create procedure Payslip
+@EmployeeID int
 as
 begin
-    declare @HRA decimal(10, 2)
-    declare @DA decimal(10, 2)
-    declare @PF decimal(10, 2)
-    declare @IT decimal(10, 2)
-    declare @Deductions decimal(10, 2)
-    declare @GrossSalary decimal(10, 2)
-    declare @NetSalary decimal(10, 2)
+    declare @EmployeeName varchar(255)
+    declare @Sal int
+    declare @HRA int
+    declare @DA int
+    declare @PF int
+    declare @IT int
+    declare @Deductions int
+    declare @GrossSalary int
+    declare @NetSalary int
 
-    -- Calculate HRA, DA, PF, and IT
-    set @HRA = 0.10 * @Salary
-    set @DA = 0.20 * @Salary
-    set @PF = 0.08 * @Salary
-    set @IT = 0.05 * @Salary
+-- fetching employee details
+select @EmployeeName = Ename, @Sal = Sal
+from employees
+where EmpNo = @EmployeeID
 
-    -- Calculate Deductions
-    set @Deductions = @PF + @IT
+-- Calculate HRA, DA, PF, IT
+set @HRA = @Sal * 0.10
+set @DA = @Sal * 0.20
+set @PF = @Sal * 0.08
+set @IT = @Sal * 0.05
 
-    -- Calculate Gross Salary
-    set @GrossSalary = @Salary + @HRA + @DA
+-- Calculate Deductions
+set @Deductions = @PF + @IT
 
-    -- Calculate Net Salary
-    set @NetSalary = @GrossSalary - @Deductions
+-- Calculate Gross Salary
+set @GrossSalary = @Sal + @HRA + @DA
 
-    -- Display the payslip
-    select 'Employee ID: ' + CAST(@EmployeeID AS VARCHAR(10)) AS 'Payslip',
-           'Basic Salary: ' + CAST(@Salary AS VARCHAR(10)) AS ' ',
-           'HRA: ' + CAST(@HRA AS VARCHAR(10)) AS ' ',
-           'DA: ' + CAST(@DA AS VARCHAR(10)) AS ' ',
-           'PF: ' + CAST(@PF AS VARCHAR(10)) AS ' ',
-           'IT: ' + CAST(@IT AS VARCHAR(10)) AS ' ',
-           'Deductions: ' + CAST(@Deductions AS VARCHAR(10)) AS ' ',
-           'Gross Salary: ' + CAST(@GrossSalary AS VARCHAR(10)) AS ' ',
-           'Net Salary: ' + CAST(@NetSalary AS VARCHAR(10)) AS ' ';
-END
-EXEC GeneratePayslip @EmployeeID = 1, @Salary = 1200000.00;
+-- Calculate Net Salary
+set @NetSalary = @GrossSalary - @Deductions
+
+-- Print the payslip
+print 'Employee Name: ' + @EmployeeName
+print 'Salary: ' + cast (@Sal as varchar (255))
+print 'HRA: ' + cast (@HRA as varchar (255))
+print 'DA: ' + cast (@DA as varchar (255))
+print 'PF: ' + cast (@PF as varchar (255))
+print 'IT: ' + cast (@IT as varchar (255))
+print 'Deductions: ' + cast (@Deductions as varchar (255))
+print 'Gross Salary: ' + cast (@GrossSalary as varchar (255))
+print 'Net Salary: ' + cast (@NetSalary as varchar (255))
+end
+-- Execute the procedure to generate a payslip for employee with EmpNo 7369
+exec Payslip @EmployeeID = 7902
