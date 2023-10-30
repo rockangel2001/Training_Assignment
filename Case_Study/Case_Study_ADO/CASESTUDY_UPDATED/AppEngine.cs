121584264f5be5c17ba380b040069e415696abfa
Shreya Mishra <shreyam@infinite.com>
2023-10-30 09:54:36 +0530

    ADO

--- Case_Study/Case_Study_ADO/CASESTUDY_UPDATED/AppEngine.cs
+++ Case_Study/Case_Study_ADO/CASESTUDY_UPDATED/AppEngine.cs
@@ -21,17 +21,7 @@ namespace Case_Study
         }
         private void InitializeData()
         {
-            //students
-            //students.Add(new Student(1033404, "SHREYANK", "2001-05-29"));
-
-            //course
-            //courses.Add(new Course(100, "Computer science and engg"));
-            //courses.Add(new Course(101, "Information science and engg"));
-            //courses.Add(new Course(102, "Electronics and communication"));
-            //courses.Add(new Course(103, "Electrical and electronics engg"));
-            //courses.Add(new Course(104, "Mechanical engg"));
-            //courses.Add(new Course(104, "civil engg"));
-
+            
 
         }
 
