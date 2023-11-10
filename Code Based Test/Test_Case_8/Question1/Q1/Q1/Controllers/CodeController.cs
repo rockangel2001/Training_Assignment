c8e3e7be6ad70452a0e5586778cddf98d484e466
Shreya Mishra <shreyam@infinite.com>
2023-11-10 18:23:18 +0530

    CodeBase8

--- Code Based Test/Test_Case_8/Question1/Q1/Q1/Controllers/CodeController.cs
+++ Code Based Test/Test_Case_8/Question1/Q1/Q1/Controllers/CodeController.cs
@@ -8,10 +8,16 @@ namespace Q1.Controllers
 {
     public class CodeController : Controller
     {
+        private NorthwindEntities db = new NorthwindEntities();
         // GET: Code
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult CustomersInGermany()
+        {
+            var germanyCustomers = db.Customers.Where(c => c.Country == "Germany").ToList();
+            return View(germanyCustomers);
         }
     }
 }
